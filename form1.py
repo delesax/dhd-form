f4bac9d1fd15f9335e4640302c540fb743d2eec4
delesax <65947178+delesax@users.noreply.github.com>
2026-03-05 15:09:23 +0000

    Update form1.py

--- form1.py
+++ form1.py
@@ -330,7 +330,9 @@ with c3:
         market_type = selectbox_with_placeholder("Public/Private", ["Public", "Private"], key="market_type")
 with c4:
 	related_systems = None
-    if product != "Market Data / Vendor Data":
+    if product == "Market Data / Vendor Data":
+		related_systems = None
+	else:
 		related_systems = selectbox_with_placeholder("Related System",RELATED_SYSTEM.get(product, []),key="related_system")
 
 
@@ -573,6 +575,7 @@ if submitted:
                 "Attachment Names": attachment_names,
                 "Template Downloaded": template_downloaded,
         })
+
 
 
 
